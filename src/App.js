30579cc594db93d7c80a524b888950b2b5e4236a
Rishi Ajoykumar <rishiajoykumar@MacBook-Pro-10.local>
2024-07-28 14:32:30 -0500

    completed functionality of CurrencySelectorComponent.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import { fetchExchangeRates } from './services/exchangeRateApi';
+import { fetchExchangeRate, fetchCurrencyCodes } from './services/exchangeRateApi';
 import { useState, useEffect } from 'react';
+import CurrencySelectorComponent from './components/currency-selector-component';
 
 function App() {
   const [exchangeRate, setExchangeRate] = useState('');
-
-  async function getExchangeRates() {
-    try {
-      const conversion_rate = await fetchExchangeRates('USD', 'EUR');
-      setExchangeRate(conversion_rate);
-    } catch (error) {
-      console.error("Error fetching exchange rates:", error);
-    }
-  }
+  const [currencyCodes, setCurrencyCodes] = useState([]); 
+  const [selectedBaseCurrency, setSelectedBaseCurrency] = useState('USD');
+  const [selectedTargetCurrency, setSelectedTargetCurrency] = useState('EUR');
 
   useEffect(() => {
-    getExchangeRates();
-  }, []); 
+    async function fetchData() {
+      try {
+        const codes = await fetchCurrencyCodes();
+        setCurrencyCodes(codes);
+        const rate = await fetchExchangeRate(selectedBaseCurrency, selectedTargetCurrency);
+        setExchangeRate(rate);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, [selectedBaseCurrency, selectedTargetCurrency]);
+
+  function handleBaseCurrencyChange(event) {
+    setSelectedBaseCurrency(event.target.value);
+  }
+
+  function handleTargetCurrencyChange(event) {
+    setSelectedTargetCurrency(event.target.value);
+  }
 
   return (
     <div className="App">
-      <h1>Exchange Rate: ${exchangeRate}</h1>
-      <button onClick={getExchangeRates}> 
-        Get Exchange Rate
-      </button>
+      <h1>Exchange Rate: {exchangeRate}</h1>
+      <CurrencySelectorComponent
+        codes={currencyCodes}
+        selectedCurrency={selectedBaseCurrency}
+        onChange={handleBaseCurrencyChange}
+        label="Base Currency"
+      />
+      <CurrencySelectorComponent
+        codes={currencyCodes}
+        selectedCurrency={selectedTargetCurrency}
+        onChange={handleTargetCurrencyChange}
+        label="Target Currency"
+      />
     </div>
   );
 }
